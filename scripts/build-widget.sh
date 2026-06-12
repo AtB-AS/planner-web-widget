723e571adfd46740a79e4a8e8258008cdf5f9f4f
Johannes Røsvik <j.rosvik@gmail.com>
2026-06-12 11:47:26 +0200

    build: use pnpm as package manager

--- scripts/build-widget.sh
+++ scripts/build-widget.sh
@@ -9,7 +9,7 @@ cd "$SCRIPT_DIR"
 if [ -n "$ORG_ID" ]; then
     ORG_ID="$ORG_ID"
     echo "Found ORG_ID=$ORG_ID in environment. Building widget for $ORG_ID"
-    ORG_ID=$ORG_ID yarn build
+    ORG_ID=$ORG_ID pnpm run build
 else
   echo "No ORG_ID found in environment. Please expose it, or build all widgets instead"
   exit 1
